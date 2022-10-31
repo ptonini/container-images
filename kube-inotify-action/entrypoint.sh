0fd2752ccf74f5d34584ed1f69698641ba967eb9
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-10-30 22:19:08 -0300

    chore(kube-inotify-action): first release
    Release-As(kube-inotify-action): 1.0.0

---
+++ kube-inotify-action/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+[ -n "${RUNTIME_FLAGS}" ] && set "${RUNTIME_FLAGS}"
+[ "${DEBUG}" = "true" ] && set -x +e
+
+[ "${WAIT_TIME}" != "0" ] && sleep "${WAIT_TIME}"
+while true; do
+    inotifywait "${INOTIFY_ARGS}" "${INOTIFY_TARGET}" || true
+    sh -c "${ACTION}"
+done
