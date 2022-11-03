0543e8c9a4ce14403a9aeffaec6e122d0c485124
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-11-02 21:22:55 -0300

    chore: removed botman and object-update-action images

--- object-update-action/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-[ -n "${RUNTIME_FLAGS}" ] && set "${RUNTIME_FLAGS}"
-[ "${DEBUG}" = "true" ] && set -x +e
-
-while true; do
-    inotifywait "${INOTIFY_ARGS}" "${OBJECT}" || true
-    sh -c "${ACTION}"
-done
