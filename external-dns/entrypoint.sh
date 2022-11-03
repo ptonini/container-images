3337efad1086560fcc804152d13730783945e256
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-11-02 21:37:53 -0300

    chore(external-dns): refactored Dockerfile and entrypoint.sh

--- external-dns/entrypoint.sh
+++ external-dns/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
-[ -n "${RUNTIME_FLAGS}" ] && set "${RUNTIME_FLAGS}"
-[ "${DEBUG}" = "true" ] && set -x +e
 
+[ "${DEBUG}" = "true" ] && RUNTIME_FLAGS="-x +e ${RUNTIME_FLAGS}"
+[ -n "${RUNTIME_FLAGS}" ] && set "${RUNTIME_FLAGS}"
+
+COMMAND="/bin/external-dns"
+
+${COMMAND} "${@}" &
+sleep 3
 while true; do
-    killall external-dns || true
-    /bin/external-dns "${@}" &
-    inotifywait "${INOTIFY_ARGS}" "${TARGET}" || true
+    killall ${COMMAND}
+    ${COMMAND} "${@}" &
+    inotifywait "${INOTIFY_ARGS}" "${INOTIFY_TARGET}" || true
 done
